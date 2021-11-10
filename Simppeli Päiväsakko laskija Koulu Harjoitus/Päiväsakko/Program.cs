6473d5bee3ce8a6df8a797e0a7f42de98c8afaf6
TemeFi <teemukarhunen95@gmail.com>
2021-11-10 20:00:57 +0200

    Valmis

---
+++ Simppeli Päiväsakko laskija Koulu Harjoitus/Päiväsakko/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Päiväsakko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Teemu Karhunen
+            decimal nettotulo = 0m;
+            int lapset = 0;
+            decimal paivasakko = 0m;
+
+            Console.WriteLine("Mikä on nettotulosi? ");
+            nettotulo = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Laten määrä ");
+            lapset = int.Parse(Console.ReadLine());
+
+            paivasakko = (nettotulo - 255) / 60 - lapset * 3;
+
+            Console.WriteLine("Päiväsakkosi on " + (int)paivasakko + " euroa");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Simppeli kilometri laskija Koulu Harjoitus/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = 0;
+            int min = 0;
+            int km = 0;
+            int aika = 0;
+
+
+
+            Console.WriteLine("Montako minuuttia menee kilometrin ajamiseen?");
+            min = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Montako kilometriä ajat?");
+            km = int.Parse(Console.ReadLine());
+
+            aika = km * min;
+
+            while (aika >= 60)
+            {
+                    h = h + 1;
+                    aika = aika - 60;
+                    min = aika;
+            }
+               Console.WriteLine(h + " Tuntia " + min + " minuuttia ");
+
+
+            Console.ReadKey();
+        }
+    }
+}
